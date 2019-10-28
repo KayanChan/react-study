875b24c2a432d0d686a129f84bd05d3f768b30aa
KayanChan <1010136337@qq.com>
2019-10-28 23:52:13 +0800

    fetch简单封装

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,57 +1,37 @@
-const BASE_URL = ''
+import { message } from 'antd'
+let BASE_URL = ''
 
-if(process.env.NODE_ENV == 'development'){
-  BASE_URL = 'http://localhost:3000'
-}else if(process.env.NODE_ENV == 'production'){
-  BASE_URL = ''
+function parseJSON(responese) {
+  return JSON.parse(responese)
 }
 
-function devHttp() {
-  return {
-    env: 'dev'
+function checkStatus(responese) {
+  if(response.status >= 200 && response.status < 500){
+    return response
   }
+  const error = new Error(response.statusText)
+  error.response = response
+  throw error
 }
 
-function testHttp() {
-  return {
-    env: 'test'
+export default  function request(options = {}){
+  const { data, url } = options
+  options = {...options}
+  options.mode = 'cors'
+  delete options.url
+  if(data){
+    delete options.data
+    options.body = JSON.stringify({
+      data
+    })
   }
+  options.headers={
+    'Content-Type':'application/json'
+  }
+  // credentials让浏览器发送包含凭据的请求（即使是跨域源）
+  return fetch(BASE_URL + url, options, {credentials: 'include'})
+    .then(checkStatus)
+    .then(parseJSON)
+    .catch(err => (
+      message.error('请求失败(error:${err})')))
 }
-
-// export default {
-//   http: (process.env.NODE_ENV == 'development') ? devHttp : testHttp
-// }
-
-
-// function parseJSON(responese) {
-//   return responese.json()
-// }
-
-// function checkStatus(responese) {
-//   if(response.status >= 200 && response.status < 500){
-//     return response
-//   }
-//   const error = new Error(response.statusText)
-//   error.response = response
-//   throw error
-// }
-
-// export default  function request(options = {}){
-//   const { data, url } = options
-//   options = {...options}
-//   options.mode = 'cors'
-//   delete options.url
-//   if(data){
-//     delete options.data
-//     options.body = JSON.stringify({
-//       data
-//     })
-//   }
-//   options.headers={
-//     'Content-Type':'application/json'
-//   }
-//   return fetch(BASE_URL + url, options, {credentials: 'include'})
-//     .then(checkStatus)
-//     .then(parseJSON)
-//     .catch(err => ({err}))
-// }
